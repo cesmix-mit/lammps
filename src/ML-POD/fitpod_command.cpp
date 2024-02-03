8003696bd8847fb16401ec5a868c1cd2aaccc84f
exapde <exapde@gmail.com>
2024-02-02 20:24:35 -0500

    fix a bug related to energy for Nj=0

--- src/ML-POD/fitpod_command.cpp
+++ src/ML-POD/fitpod_command.cpp
@@ -89,7 +89,7 @@ void FitPOD::command(int narg, char **arg)
 
     // compute POD coefficients using least-squares method
 
-    least_squares_fit(traindata);
+    if (coeff_file == "") least_squares_fit(traindata);
     //error->all(FLERR, "stop after least_squares_fit");
 
     // calculate errors for the training data set
